ad414069d6d9157fa904cebcdea75d56b1a4420b
Vaibhavi patel <patelvaibhavi2412@gmail.com>
2024-06-27 19:08:29 +0530

    Updates

---
+++ precache-manifest.3bfe66f3dea9d9e0bfc45a11ea679a21.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e13205054d2c5a75e2b21586bbb9e173",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "fa3b05cdb70ff29db5da",
+    "url": "/todolist/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "6fec53887f0495aaaae0",
+    "url": "/todolist/static/js/2.b96e5f81.chunk.js"
+  },
+  {
+    "revision": "e7f7d355750eacac733d08a2eda67e17",
+    "url": "/todolist/static/js/2.b96e5f81.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c80be8c1a8bbcb899f6e",
+    "url": "/todolist/static/js/3.39aea73c.chunk.js"
+  },
+  {
+    "revision": "fa3b05cdb70ff29db5da",
+    "url": "/todolist/static/js/main.5e7ee59b.chunk.js"
+  },
+  {
+    "revision": "fd533c53af78a67b9df3",
+    "url": "/todolist/static/js/runtime-main.48714635.js"
+  }
+]);
